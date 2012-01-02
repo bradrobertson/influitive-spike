09c534606e34057e125d0534dcd4e0cce8046cda
Brad Robertson <bradleyrobertson@gmail.com>
2012-01-01 21:45:49 -0500

    wysiwyg now works, editing of templates setup

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -3,8 +3,28 @@
 *   @file admin.js
 *
 */
-!function($){
- 
+$(function(){
   
+  $("td.rightcol button").overlay({
+    // some mask tweaks suitable for modal dialogs
+  	mask: {
+  		color: '#ebecff',
+  		loadSpeed: 200,
+  		opacity: 0.9
+  	},
+  	onLoad: function(e){
+  	  e.target.getOverlay().find("textarea").htmlarea({
+        // Override/Specify the Toolbar buttons to show
+        toolbar: [
+          ["html"],
+          ["bold", "italic", "underline"],
+          ["p", "|", "h1", "h2"],
+          ["justifyLeft", "justifyCenter", "justifyRight"],
+          ["link", "unlink"]
+        ]
+      });
+  	}
+  });
   
-}(jQuery);
+  $("ul.tabs").tabs("div.panes > div"); 
+});
