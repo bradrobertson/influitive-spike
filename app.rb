6e4ddbdf2947ae0a54cd4691a8ed8c6d7d8686cc
bradrobertson <brad@influitive.com>
2012-06-04 16:10:48 -0400

    playing with bootstrap

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ get '/' do
   erb :app
 end
 
-%w{admin email templates whitelabel challenge}.each do |page|
+%w{admin advocate}.each do |page|
   get "/#{page}" do
     erb page.to_sym
   end
@@ -13,4 +13,8 @@ end
 
 get '/app.css' do
   scss :'stylesheets/app'
+end
+
+get '/bootstrap.css' do
+  scss :'stylesheets/bootstrap'
 end
--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -4,27 +4,5 @@
 *
 */
 $(function(){
-
-  $("button[rel]").overlay({
-    // some mask tweaks suitable for modal dialogs
-    mask: {
-      color: '#ebecff',
-      loadSpeed: 200,
-      opacity: 0.9
-    },
-    onLoad: function(e){
-      e.target.getOverlay().find("textarea").htmlarea({
-        // Override/Specify the Toolbar buttons to show
-        toolbar: [
-          ["html"],
-          ["bold", "italic", "underline"],
-          ["p", "|", "h1", "h2"],
-          ["justifyLeft", "justifyCenter", "justifyRight"],
-          ["link", "unlink"]
-        ]
-      });
-    }
-  });
-
-  $("ul.tabs").tabs("div.panes > div");
+  $('.dropdown-toggle').dropdown();
 });
